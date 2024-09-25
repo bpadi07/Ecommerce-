595265b988e45a72fbc9c0cc5310952563d880f6
Snehalmahasagar <snehalmahasagar@gmail.com>
2024-09-25 11:47:37 +0530

    Added purple border to chatbot, bold Collab Vision heading, and improved chatbot features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import LetsChatComponent from "./components/chatbot/LetsChat.jsx"; // Import the LetsChatComponent
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="container py-3 mx-auto flex-grow">
+        {/* This is where the routed components will be rendered */}
         <Outlet />
+        
+        {/* Toast notifications */}
         <ToastContainer />
+
+        {/* Add the LetsChatComponent here */}
+        <LetsChatComponent /> {/* Chat functionality */}
       </main>
+      
       <Footer />
     </div>
   );
