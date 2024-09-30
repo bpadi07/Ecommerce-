623d547de8eca083ff7b97529501d77304c7679d
sakshijadhav10 <131511233+sakshijadhav10@users.noreply.github.com>
2024-09-30 13:37:14 +0530

    change for stripe

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =" ";
- export const BACKEND_URL = "https://ecommerceapp1-lfnc.onrender.com";
+export const BACKEND_URL = "https://ecommerceapp1-lfnc.onrender.com";
 
 export const PRODUCTS_URL = `/api/products`;
 export const ORDERS_URL = `/api/orders`;
